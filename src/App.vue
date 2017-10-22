d74fa7180464fe6c5c9ab702297959dea3090eca
darren <764190362@qq.com>
2017-10-22 08:48:16 +0800

    搭好框架

---
+++ src/App.vue
@@ -0,0 +1,24 @@
+<template>
+    <div class="container">
+        <app-quote-grid :quotes="quotes"></app-quote-grid>
+    </div>
+</template>
+
+<script>
+    import QuoteGrid from './components/QuoteGrid.vue';
+    export default {
+        data: function(){
+            return {
+                maxQuotes: 10,
+                quotes: ['Just a Quote Test']
+            };
+        },
+        components: {
+            appQuoteGrid: QuoteGrid
+        }
+    }
+</script>
+
+<style>
+
+</style>
